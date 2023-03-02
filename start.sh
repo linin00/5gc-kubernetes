5a403015710c365199bf5d37265f5f01c4b17a1e
linin00 <lianjie35@outlook.com>
2023-03-02 14:17:45 +0800

    init test

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
+microk8s.kubectl apply -f ./unix-daemonset.yaml
 microk8s.kubectl apply -f ./mongo/mongo-pv.yaml
 microk8s.kubectl apply -f ./mongo/mongo-pvc.yaml
-microk8s.kubectl apply -f ./unix-daemonset.yaml
 microk8s.kubectl apply -f ./mongo/5gc-mongo.yaml
 microk8s.kubectl expose deployment db --port=27017
+
 microk8s.kubectl apply -f ./5gc-configmap.yaml
 microk8s.kubectl apply -f ./networkFunctions
 microk8s.kubectl apply -f ./webui/webui.yaml
